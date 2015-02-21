76f291e0ab68dc02c0c677dc3a962217ebb757e8
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-02-21 19:37:30 +0100

    added cardTypes and cards model

--- server/api/card/card.model.js
+++ server/api/card/card.model.js
@@ -3,10 +3,20 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var EventSchema = new Schema({
+  earned: {type: Date, default: Date.now()},
+  points: Number
+});
+
 var CardSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  created: {type: Date, default: Date.now()},
+  status: Number, // 0: inactive, 1: active
+  lastUpdated: Date,
+  user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+  merchant_id: {type: Schema.Types.ObjectId, ref: 'Merchant'},
+  cardType_id: {type: Schema.Types.ObjectId, ref: 'CardType'},
+  validFrom: {type: Date, default: Date.now()},
+  events: [EventSchema]
 });
 
 module.exports = mongoose.model('Card', CardSchema);
--- server/api/cardtype/cardtype.model.js
+++ server/api/cardtype/cardtype.model.js
@@ -3,10 +3,22 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var CardtypeSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+var EventTypeSchema = new Schema({
+  eventType: {type: String, required: true},
+  eventPoints: {type: Number, required: true}
+});
+
+var CardTypeSchema = new Schema({
+  created: {type: Date, default: Date.now()},
+  status: Number, // 0: inactive, 1: active
+  lastUpdated: Date,
+  name: {type: String, required: true},
+  description: String,
+  createdByUser_id: {type: Schema.Types.ObjectId, ref: 'User'},
+  merchant_id: {type: Schema.Types.ObjectId, ref: 'Merchant'},
+  maxPoints: Number,
+  duration: Number,
+  eventTypes: [eventTypeSchema]
 });
 
 module.exports = mongoose.model('Cardtype', CardtypeSchema);
