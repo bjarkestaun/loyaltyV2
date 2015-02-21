6a4367e95543bd3f8c9a4f56c1ec0d27fe9a3fbb
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-02-21 19:52:25 +0100

    more capitalizing T in CardType

--- server/api/cardtype/cardtype.model.spec.js
+++ server/api/cardtype/cardtype.model.spec.js
@@ -3,31 +3,31 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
-var Cardtype = require('./cardtype.model');
+var CardType = require('./cardtype.model');
 
 // var cardtypeInfo = {
 // };
 
 // var cardtype = new Cardtype(cardtypeInfo);
 
-describe('Cardtype Model', function() {
+describe('CardType Model', function() {
 
   before(function(done) {
     // Clear cardtypes before testing
-    Cardtype.remove().exec().then(function() {
+    CardType.remove().exec().then(function() {
       done();
     });
   });
 
   afterEach(function(done) {
-    Cardtype.remove().exec().then(function() {
+    CardType.remove().exec().then(function() {
       done();
     });
   });
 
   it('should begin with no cardtypes', function(done) {
-    Cardtype.find({}, function(err, cardtypes) {
-      cardtypes.should.have.length(0);
+    CardType.find({}, function(err, cardTypes) {
+      cardTypes.should.have.length(0);
       done();
     });
   });
